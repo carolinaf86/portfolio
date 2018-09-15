da7bf9008a0c518fc69154840b665c3ed9b889c8
Caroline <caroline@bluerosedev.com>
2018-09-15 15:23:37 +0200

    Styles for greyscale theme

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -55,6 +55,19 @@ module.exports = {
       {
         test: /\.js$/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
       }
     ]
   },
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './assets/app.scss'
+import './assets/app.css'
 import BootstrapVue from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 /* eslint-disable-next-line no-new */
 new Vue({
