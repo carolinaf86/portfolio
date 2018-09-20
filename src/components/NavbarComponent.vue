6342da9855bd5cb337261d14e519becdd3e67672
Caroline <caroline@bluerosedev.com>
2018-09-20 14:59:15 +0200

    Router integration and basic componentisation

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <router-link class="navbar-brand" to="/home">Home</router-link>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+              aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"/>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/projects">Projects</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contact">Contact</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/app.css'
 import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import HomeComponent from './pages/HomeComponent.vue'
+import AboutComponent from './pages/AboutComponent.vue'
+import ProjectsComponent from './pages/ProjectsComponent.vue'
+import ContactComponent from './pages/ContactComponent.vue'
 
 Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+/* Routes */
+const routes = [
+  { path: '/home', component: HomeComponent },
+  { path: '/about', component: AboutComponent },
+  { path: '/projects', component: ProjectsComponent },
+  { path: '/contact', component: ContactComponent },
+  { path: '*', redirect: '/home' }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 /* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
