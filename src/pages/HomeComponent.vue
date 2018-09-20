4fea99f3f6a5c45461f4045b6c8d9965789f48c5
Caroline <caroline@bluerosedev.com>
2018-09-20 16:46:36 +0200

    Replace footer component and move signup section to home component

--- src/pages/HomeComponent.vue
+++ src/pages/HomeComponent.vue
@@ -1,11 +1,33 @@
 <template>
-  <header class="masthead">
-    <div class="container d-flex h-100 align-items-center">
-      <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 text-uppercase">Foo!!!!!!!</h1>
-        <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
-        <router-link to="about" class="btn btn-primary">Get Started</router-link>
+  <div>
+    <header class="masthead">
+      <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+          <h1 class="mx-auto my-0 text-uppercase">Foo!!!!!!!</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
+          <router-link to="about" class="btn btn-primary">Get Started</router-link>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+
+    <!-- Signup Section -->
+    <section id="signup" class="signup-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto text-center">
+
+            <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+            <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
+
+            <form class="form-inline d-flex">
+              <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail"
+                     placeholder="Enter email address...">
+              <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
