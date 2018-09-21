115f2d1d0628257457344f1b76be25609983e865
Caroline <caroline@bluerosedev.com>
2018-09-21 16:28:47 +0200

    Add scrollspy to navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" :class="{ 'navbar-shrink': shrinkNav }" id="mainNav">
+  <b-nav class="navbar navbar-expand-lg navbar-light fixed-top" :class="{ 'navbar-shrink': shrinkNav }" v-b-scrollspy id="mainNav">
     <div class="container">
       <a class="navbar-brand" href="#" v-scroll-to="'#home'">Start Bootstrap</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,18 +8,18 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#" v-scroll-to="'#about'">About</a>
+            <a class="nav-link" href="#about" v-scroll-to="'#about'">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" v-scroll-to="'#projects'">Projects</a>
+            <a class="nav-link" href="#projects" v-scroll-to="'#projects'">Projects</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" v-scroll-to="'#contact'">Contact</a>
+            <a class="nav-link" href="#contact" v-scroll-to="'#contact'">Contact</a>
           </li>
         </ul>
       </div>
     </div>
-  </nav>
+  </b-nav>
 </template>
 <script>
 
