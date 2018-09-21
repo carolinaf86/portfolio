b471468ba35288d215ed5aff64fdf14227575184
Caroline <caroline@bluerosedev.com>
2018-09-21 16:55:34 +0200

    Header section

--- src/pages/HomeComponent.vue
+++ src/pages/HomeComponent.vue
@@ -5,13 +5,12 @@
         <div class="row">
           <div class="col-lg-10 mx-auto">
             <h1 class="text-uppercase">
-              <strong>Your Favorite Source of Free Bootstrap Themes</strong>
+              <strong>Caroline</strong>
             </h1>
             <hr>
           </div>
           <div class="col-lg-8 mx-auto">
-            <p class="text-faded mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download
-              your template and start going, no strings attached!</p>
+            <h4 class="text-faded mb-5">A frontend web developer based in Barcelona, Spain</h4>
             <a class="btn btn-primary btn-xl" href="#" v-scroll-to="'#about'" >Find Out More</a>
           </div>
         </div>
