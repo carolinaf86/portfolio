3b2e1ecd40706554b6fd2d0e3ef17cb2cb98f6ef
Caroline <caroline@bluerosedev.com>
2018-09-21 17:55:11 +0200

    Contact section content

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,21 +3,20 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 mx-auto text-center">
-          <h2 class="section-heading">Let's Get In Touch!</h2>
+          <h2 class="section-heading">Contact me</h2>
           <hr class="my-4">
-          <p class="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back
-            to you as soon as possible!</p>
+          <p class="mb-5">If you'd like to get in touch with me, I'd love to hear from you. You can find me here:</p>
         </div>
       </div>
       <div class="row">
         <div class="col-lg-4 ml-auto text-center">
           <i class="fas fa-phone fa-3x mb-3 sr-contact-1"/>
-          <p>123-456-6789</p>
+          <p>+34 610 642 683</p>
         </div>
         <div class="col-lg-4 mr-auto text-center">
           <i class="fas fa-envelope fa-3x mb-3 sr-contact-2"/>
           <p>
-            <a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a>
+            <a href="mailto:your-email@your-domain.com">caroline@bluerosedev.com</a>
           </p>
         </div>
       </div>
