9272b5164ea2a71fb6115ecef598d1ca00d38aaf
Caroline <caroline@bluerosedev.com>
2018-10-02 16:49:17 +0200

    Replace bootstrap nav with vue-bootstrap b-navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,55 +1,37 @@
 <template>
-  <b-nav class="navbar navbar-expand-lg navbar-light fixed-top" :class="{ 'navbar-shrink': shrinkNav }" v-b-scrollspy id="mainNav">
+  <b-navbar toggleable="lg" fixed="top" :class="{ 'navbar-shrink': shrinkNav }" v-b-scrollspy id="mainNav">
     <div class="container">
-      <a v-if="$route.path === '/home'" class="navbar-brand" href="#" v-scroll-to="'#home'">Home</a>
-      <router-link v-else class="navbar-brand" to="/home">Home</router-link>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-              aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
+
+      <b-navbar-brand v-if="$route.path === '/home'" href="#" v-scroll-to="'#home'">Home</b-navbar-brand>
+      <b-navbar-brand v-else >
+        <router-link to="/home">Home</router-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="navbarResponsive"></b-navbar-toggle>
+
+      <b-collapse is-nav id="navbarResponsive">
+
+        <b-navbar-nav class="ml-auto">
+
           <template v-if="$route.path === '/home'">
-            <li class="nav-item">
-              <a class="nav-link" href="#about" v-scroll-to="'#about'">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#skills" v-scroll-to="'#skills'">Skills</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#projects" v-scroll-to="'#projects'">Projects</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#contact" v-scroll-to="'#footer'">Contact</a>
-            </li>
+            <b-nav-item href="#about" v-scroll-to="'#about'">About</b-nav-item>
+            <b-nav-item href="#skills" v-scroll-to="'#skills'">Skills</b-nav-item>
+            <b-nav-item href="#projects" v-scroll-to="'#projects'">Projects</b-nav-item>
+            <b-nav-item href="#contact" v-scroll-to="'#footer'">Contact</b-nav-item>
           </template>
           <template v-if="$route.path === '/cv'">
-            <li class="nav-item">
-              <a class="nav-link" href="#name-row" v-scroll-to="'#name-row'">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#skills-row" v-scroll-to="'#skills-row'">Skills</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#experience-row" v-scroll-to="'#experience-row'">Work Experience</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#education-row" v-scroll-to="'#education-row'">Education</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#languages-row" v-scroll-to="'#languages-row'">Languages</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#references-row" v-scroll-to="'#references-row'">References</a>
-            </li>
+            <b-nav-item href="#name-row" v-scroll-to="'#name-row'">About</b-nav-item>
+            <b-nav-item href="#skills-row" v-scroll-to="'#skills-row'">Skills</b-nav-item>
+            <b-nav-item href="#experience-row" v-scroll-to="'#experience-row'">Work Experience</b-nav-item>
+            <b-nav-item href="#education-row" v-scroll-to="'#education-row'">Education</b-nav-item>
+            <b-nav-item href="#languages-row" v-scroll-to="'#languages-row'">Languages</b-nav-item>
+            <b-nav-item href="#references-row" v-scroll-to="'#references-row'">References</b-nav-item>
           </template>
-          <li class="nav-item" v-if="$route.path !== '/cv'">
-            <router-link class="nav-link" to="/cv">CV</router-link>
-          </li>
-        </ul>
-      </div>
+          <b-nav-item v-if="$route.path !== '/cv'" to="/cv">CV</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </div>
-  </b-nav>
+  </b-navbar>
 </template>
 <script>
 
