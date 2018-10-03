752d4e0f8d3c9aa09a11a44a28641a49e67c683f
Caroline <caroline@bluerosedev.com>
2018-10-03 10:26:17 +0200

    Changes to portfolio page layout and content

--- src/components/ProjectDetailComponent.vue
+++ src/components/ProjectDetailComponent.vue
@@ -3,8 +3,6 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 mx-auto text-center">
-          <router-link class="btn btn-light mr-2 mb-5" :to="previousUrl"><i class="fas fa-chevron-left"></i></router-link>
-          <router-link class="btn btn-light mb-5" :to="nextUrl"><i class="fas fa-chevron-right"></i></router-link>
           <p v-for="paragraph of text" class="text-faded mb-4">
             {{ paragraph }}
           </p>
@@ -20,18 +18,6 @@ export default {
       default: function () {
         return ['']
       }
-    }
-  },
-  computed: {
-    nextUrl: function () {
-      const { id } = this.$route.params
-      if (+id > 5) return '/home'
-      return `/projects/${+this.$route.params.id + 1}`
-    },
-    previousUrl: function () {
-      const { id } = this.$route.params
-      if (+id === 1) return '/home'
-      return `/projects/${+this.$route.params.id - 1}`
     }
   }
 }
