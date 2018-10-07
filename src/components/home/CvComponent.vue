911892e414df3ff7bfd58639d52deb4ef5603dcf
Caroline <caroline@bluerosedev.com>
2018-10-07 17:29:57 +0200

    Update build

--- src/components/home/CvComponent.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section id="cv" class="bg-dark text-white">
-    <div class="container text-center">
-      <h2 class="mb-4">Check out my CV for more details!</h2>
-      <router-link class="btn btn-light btn-xl sr-button" to="/cv" v-scroll-reveal.reset="{delay: 200}">View CV</router-link>
-    </div>
-  </section>
-</template>
