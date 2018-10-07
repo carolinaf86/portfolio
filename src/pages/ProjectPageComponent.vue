f03e4b06464d52076d3a09ad25b558e00c58cdd8
Caroline <caroline@bluerosedev.com>
2018-10-07 17:29:12 +0200

    Change portfolio background to be controlled by css to ensure paths are correct when minified

--- src/pages/ProjectPageComponent.vue
+++ src/pages/ProjectPageComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <header id="project-detail-header"
-            :style="{'background-image': `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), url(${bgImgPath})`}"
-            class="masthead project-header text-center text-white d-flex">
+            class="masthead project-header text-center text-white d-flex"
+            v-bind:class="headerClass">
       <project-detail-header :content="contentForRoute"/>
     </header>
     <project-detail-component :text="contentForRoute.text"/>
@@ -28,10 +28,8 @@ export default {
     contentForRoute: function () {
       return this.content[this.$route.params.id]
     },
-    bgImgPath: function () {
-      return +this.$route.params.id === 6
-        ? '../../static/img/header.jpg'
-        : `../../static/img/portfolio/${this.$route.params.id}.jpg`
+    headerClass: function () {
+      return +this.$route.params.id === 6 ? 'default' : `portfolio-${this.$route.params.id}`
     }
   }
 }
